f9d96112b604da8dbfe74664e7fa0fa299a28f75
ZieglerAaron <s0588522@htw-berlin.de>
2025-07-22 22:51:06 +0200

    Enhance Airflow

--- airflow/dags/trino_example_dag.py
+++ airflow/dags/trino_example_dag.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.providers.trino.operators.trino import TrinoOperator
+from airflow.operators.bash import BashOperator
 from datetime import datetime
 
 default_args = {
@@ -14,9 +15,27 @@ dag = DAG(
     catchup=False,
 )
 
-run_trino_query = TrinoOperator(
+run_trino_query = BashOperator(
     task_id='run_trino_query',
-    trino_conn_id='trino_iceberg',
-    sql="SELECT * FROM example.iceberg.sample_table LIMIT 10",
+    bash_command='curl -s -H "X-Trino-User: admin" http://controller:8080/v1/statement -X POST -H "Content-Type: application/json" -d \'{"query": "SHOW CATALOGS"}\'',
+    dag=dag,
+)
+
+# Task zum Anlegen der Iceberg-Tabelle via CTAS
+create_iceberg_table = BashOperator(
+    task_id='create_iceberg_table',
+    bash_command='''curl -s -H "X-Trino-User: admin" http://controller:8080/v1/statement -X POST -H "Content-Type: application/json" -d '{
+      "query": "CREATE TABLE IF NOT EXISTS iceberg.default.testdaten WITH (format = \\"PARQUET\\", location = \\"s3a://wba/parquet/testdaten_table/\\") AS SELECT * FROM hive.default.\\"s3a://wba/parquet/Testdaten.parquet\\""
+    }' ''',
+    dag=dag,
+)
+
+# Task zum Preview der neu angelegten Tabelle
+preview_iceberg_table = BashOperator(
+    task_id='preview_iceberg_table',
+    bash_command='curl -s -H "X-Trino-User: admin" http://controller:8080/v1/statement -X POST -H "Content-Type: application/json" -d \'{"query": "SELECT * FROM iceberg.default.testdaten LIMIT 10"}\'',
     dag=dag,
 ) 
+
+# Abhängigkeiten
+run_trino_query >> create_iceberg_table >> preview_iceberg_table 
